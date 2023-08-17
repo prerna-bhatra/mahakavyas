fe1bcbb1a2ad9206e9bebfeac8f293d0780d64a1
p.bhatra@thefifthelement.co <p.bhatra@thefifthelement.co>
2023-08-17 11:25:43 +0530

    request

--- src/puran/puran.controller.ts
+++ src/puran/puran.controller.ts
@@ -6,25 +6,25 @@ export class PuranController {
 
     constructor(private puranservice: PuranService) { }
     @Post()
-    async create(@Body() puranBody, @Req() request: Request) {
+    async create(@Body() puranBody) {
         return await this.puranservice.addPuran(puranBody)
 
     }
 
     @Post('chapter')
-    async createChapter(@Body() chapterBody, @Req() request: Request) {
+    async createChapter(@Body() chapterBody) {
         return await this.puranservice.addChapter(chapterBody)
 
     }
 
     @Post('chapterByPuran')
-    async chaptersByPuranId(@Body() chapterBody, @Req() request: Request) {
+    async chaptersByPuranId(@Body() chapterBody) {
         const { puranId } = chapterBody;
         return await this.puranservice.getChaptersByPuranId(puranId)
     }
 
     @Post('chapterById')
-    async chapterById(@Body() chapterBody, @Req() request: Request) {
+    async chapterById(@Body() chapterBody) {
         const { chapterId } = chapterBody;
         return await this.puranservice.getChaptersById(chapterId)
 
